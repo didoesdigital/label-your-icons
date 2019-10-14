13cce1dcc02dc42eb1bd34e8864671d021c6d1cd
Di <didoesdigital+git@gmail.com>
2019-10-14 14:50:09 +1100

    Tweak copy and add nowrap to avoid orphan text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Label Your Icons" />
-    <p className="lede">There is a scourge taking over digital design: the unlabelled icon.</p>
+    <p className="lede">There is a scourge taking over digital design: icons <span className="nowrap">without labels</span>.</p>
     <p className="lede">You can help put an end to this bewildering practice. Add a label to your icon. Add labels to <strong>all</strong> your icons.</p>
   </Layout>
 )
