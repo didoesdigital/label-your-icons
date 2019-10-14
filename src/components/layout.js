7a2ca0d55a91e54ac9db7f42212586b91b74bde7
Di <didoesdigital+git@gmail.com>
2019-10-14 15:59:45 +1100

    Add home page content and styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,18 +26,14 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
+      <main>{children}</main>
+      <footer className="bg-daintree reversed landing-page-section">
+        <div className="mx-auto mw-1024 p3">
           <p>
-            © {new Date().getFullYear()} Diana MacDonald. Built with
+            © {new Date().getFullYear()} Diana MacDonald.
+          </p>
+          <p>
+            Built with
             {` `}
             <a href="https://www.gatsbyjs.org">Gatsby</a>.
           </p>
@@ -45,13 +41,13 @@ const Layout = ({ children }) => {
             <a style={{ marginLeft: `2rem` }} className="svg-icon-wrapper svg-baseline" aria-label="GitHub" href="https://github.com/didoesdigital/label-your-icons">
               <svg xmlns="http://www.w3.org/2000/svg" role="presentation" viewBox="0 0 512 512" style={{ marginLeft: `-1.25rem` }}>
                 <rect width="512" height="512" rx="0" fill="transparent"/>
-                <path fill="#032535" d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"/>
+                <path fill="#fff" d="M335 499c14 0 12 17 12 17H165s-2-17 12-17c13 0 16-6 16-12l-1-50c-71 16-86-28-86-28-12-30-28-37-28-37-24-16 1-16 1-16 26 2 40 26 40 26 22 39 59 28 74 22 2-17 9-28 16-35-57-6-116-28-116-126 0-28 10-51 26-69-3-6-11-32 3-67 0 0 21-7 70 26 42-12 86-12 128 0 49-33 70-26 70-26 14 35 6 61 3 67 16 18 26 41 26 69 0 98-60 120-117 126 10 8 18 24 18 48l-1 70c0 6 3 12 16 12z"/>
               </svg>
               GitHub
             </a>
           </p>
-        </footer>
-      </div>
+        </div>
+      </footer>
     </>
   )
 }
